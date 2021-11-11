07a1103ba63b33bc82a467b975d2894eb050356e
j-adamcic <janina.adamcic@gmail.com>
2021-11-11 18:23:58 +0100

    new author submit form now actually creates authors

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,4 +1,17 @@
 class AuthorsController < ApplicationController
     def new
     end
+
+    def create
+        #render plain: params[:author].inspect
+        @author = Author.new(author_params)
+
+        @author.save
+        redirect_to root_path, notice: 'Success!'
+    end
+
+    private
+        def author_params
+            params.require(:author).permit(:first_name, :last_name, :homepage)
+        end
 end
