9142b65fc6b2a302b8343bb18614b6e71050ae4c
j-adamcic <janina.adamcic@gmail.com>
2021-11-15 16:10:56 +0100

    paper show page displays authors

--- spec/views/papers/show.html.erb_spec.rb
+++ spec/views/papers/show.html.erb_spec.rb
@@ -1,18 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe "papers/show", type: :view do
-  before(:each) do
-    @paper = assign(:paper, Paper.create!(
-      title: "Title",
-      venue: "Venue",
-      year: 2
-    ))
-  end
+  context "with paper from author named Alan Turing" do
+    alan = FactoryBot.create :author
+    
+    before(:each) do
+      @paper = assign(:paper, Paper.create!(
+        title: "Title",
+        venue: "Venue",
+        year: 2
+      ))
+      @paper.authors.append(alan)
+    end
 
-  it "renders attributes in <p>" do
-    render
-    expect(rendered).to match(/Title/)
-    expect(rendered).to match(/Venue/)
-    expect(rendered).to match(/2/)
+    it "renders attributes in <p>" do
+      render
+      expect(rendered).to match(/Title/)
+      expect(rendered).to match(/Venue/)
+      expect(rendered).to match(/2/)
+      expect(rendered).to have_text(alan.name)
+    end
   end
 end
